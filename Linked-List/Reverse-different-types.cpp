a9073b217c069b2cb59352a588b6068744a58828
PratikSaha198 <pratiksaha198@gmail.com>
2021-04-14 22:09:11 +0530

    Linked List final files

--- Linked-List/Reverse-different-types.cpp
+++ Linked-List/Reverse-different-types.cpp
@@ -39,3 +39,45 @@ ListNode *reverseBetween(ListNode *head, int left, int right)
     }
     return dum.next;
 }
+
+
+// Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+// k is a positive integer and is less than or equal to the length of the linked list. 
+// If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [2,1,4,3,5]
+
+// Function to reverse from a starting node to a end node is made. It returns the starting of the list
+// For every next k elements recursively keep on revsersing by passing the newhead (after k elements) and keep on returning it.
+
+
+ListNode* reverse(ListNode* f, ListNode* l)
+{
+    ListNode* curr=f;
+    ListNode *prev=NULL, *nex=NULL;
+
+    while(curr!=l){
+        nex = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nex;
+    }
+    f = prev;
+
+    return f;
+}
+ListNode* reverseKGroup(ListNode* head, int k)
+{
+    auto last = head;
+
+    for(int i=0;i<k;i++){
+        if(!last) return head;
+        last = last->next;
+    }
+
+    auto newhead = reverse(head, last);
+    head->next = reverseKGroup(last, k);
+    
+    return newhead;
+}
